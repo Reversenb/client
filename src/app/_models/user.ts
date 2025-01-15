a42f071618eca80aafa94b44581ee07810ad67ac
Reverse7K <zakoki711@gmail.com>
2025-01-15 12:05:27 +0700

    update

---
+++ src/app/_models/user.ts
@@ -0,0 +1,24 @@
+import { Photo } from "./photo"
+
+export interface User {
+    id?: string,
+    display_name?: string,
+    username: string,
+    create_at?: Date,
+    updated_at?: Date,
+    last_active?: Date,
+    introduction?: string,
+    interest?: string,
+    looking_for?: string,
+    location?: string,
+    gender?: string,
+    age?: string,
+    avatar?: string,
+    photos?: Photo[],
+    photoOfTheDay?: string,
+
+    //Like Feature
+    followers: User[] | String[],
+    following: User[] | String[],
+
+}
